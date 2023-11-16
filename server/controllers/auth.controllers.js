3a30faa67e890ec43cfde325332a5f60cc5d4531
Saheel Das <saheelkumar.das@bajajfinserv.in>
2023-11-17 02:25:11 +0530

    fixing bcryptjs import

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 const { User } = require("../models/user.model");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 exports.getUserDataController = async (req, res) => {
     try {
