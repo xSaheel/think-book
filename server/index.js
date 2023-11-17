9103ea96d3a817f6b673c4b7b5c644deedaf7abd
Saheel Das <saheelkumar.das@bajajfinserv.in>
2023-11-17 23:19:31 +0530

    increasing memory from server

--- server/index.js
+++ server/index.js
@@ -18,14 +18,14 @@ const corsOptions = {
     credentials: true
 };
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb'}));
 app.use(cors(corsOptions));
 
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/auth", authRoutes);
 
 //testing
-app.use("/", (req, res) => res.send("Hello World"))
+app.use("/", (req, res) => res.send("Think-book Servers"))
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
