63fb9040be896c523d3fcd229badc1831f5c8ef3
Saheel Das <saheelkumar.das@bajajfinserv.in>
2023-11-21 15:32:42 +0530

    Added loader for router events

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import AuthProvider from "@/providers/auth.provider";
+import NextNProgress from "nextjs-progressbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <NextNProgress />
       <Component {...pageProps} />
     </AuthProvider>
   );
